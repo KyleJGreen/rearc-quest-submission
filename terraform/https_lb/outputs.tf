a2a0cccf045a0aad5e1cac63efb28c9a1c6178d2
kgreen <kylegreen294@gmail.com>
2025-08-03 14:57:03 -0600

    add comments for outputs and vars

--- terraform/https_lb/outputs.tf
+++ terraform/https_lb/outputs.tf
@@ -1,3 +1,7 @@
 output "quest_webapp_ip_address" {
+    description = <<EOF
+The IP address of the load balancer. This will be used to access the web
+application.
+EOF
   value = data.google_compute_global_address.quest_webapp_ip.address
 }
--- terraform/https_lb/variables.tf
+++ terraform/https_lb/variables.tf
@@ -5,7 +5,15 @@ variable "project_id" {
 }
 
 variable "url_map_id" {
-  description = "The ID of the URL map to be used by the HTTPS proxy."
+  description = <<EOF
+The ID of the URL map to be used by the HTTPS proxy.
+In the current setup, this is hardcoded to the URL map
+created in the quest_webapp module. If the modules were to be
+consolidated, this could be replaced with a reference to the field of
+the URL map created in the same module. The terraform provider
+does not support referencing the URL map as a data source.
+EOF
+
   type        = string
   default = "https://www.googleapis.com/compute/v1/projects/rearc-quest-kgreen/global/urlMaps/quest-webapp-url-map"
 }
